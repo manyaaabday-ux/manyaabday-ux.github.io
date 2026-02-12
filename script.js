1bdb37454180b17f51366753a6b5016bc6540483
manyaaabday-ux <manyaaabday@gmail.com>
2026-02-12 23:12:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ let currentPage = 0;
 const pages = document.querySelectorAll(".page");
 
 function showPage(index) {
-    pages.forEach(page => page.classList.remove("active"));
-    pages[index].classList.add("active");
+  pages.forEach(page => page.classList.remove("active"));
+  pages[index].classList.add("active");
 }
 
 function nextPage() {
-    if (currentPage < pages.length - 1) {
-        currentPage++;
-        showPage(currentPage);
-    }
+  if (currentPage < pages.length - 1) {
+    currentPage++;
+    showPage(currentPage);
+  }
 }
 
 function playMusic() {
-    const music = document.getElementById("bgMusic");
-    music.play();
+  const music = document.getElementById("bgMusic");
+  music.play();
 }
 
 function blowCandle() {
-    const flame = document.getElementById("flame");
-    const message = document.getElementById("finalMessage");
+  const flame = document.getElementById("flame");
+  const message = document.getElementById("finalMessage");
 
-    flame.style.display = "none";
-    message.style.display = "block";
+  flame.style.display = "none";
+  message.style.display = "block";
 }
