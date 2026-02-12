8f0c8f310c944f416729a32bd3dc6fb91513b1a6
manyaaabday-ux <manyaaabday@gmail.com>
2026-02-12 22:47:12 +0530

    Add page navigation and music functionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-scri
+let currentPage = 0;
+const pages = document.querySelectorAll(".page");
+
+function nextPage() {
+  pages[currentPage].classList.remove("active");
+  currentPage++;
+  if (currentPage < pages.length) {
+    pages[currentPage].classList.add("active");
+  }
+}
+
+function playMusic() {
+  document.getElementById("bgMusic").play();
+}
+
+function blowCandle() {
+  document.getElementById("flame").style.display = "none";
+  document.getElementById("finalMessage").style.display = "block";
+}
